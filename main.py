df5d364b88bd1b2eddfa7d95ac24313c8d4b1b00
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-21 13:30:33 -0500

    doc: added documentation to celsius to fahrenheit

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5 ) +32
+    """convert a temperature from celsius to fahrenheit
+
+    Args:
+        celsius (float): the temperature in degrees celsius
+
+    Returns:
+        float: the temperature converted to degrees fahrenheit
+    """
+    return (celsius * 9/5 ) + 32
 
 def test_celsius_to_fahrenheit():
     assert celsius_to_fahrenheit(100) == 212
